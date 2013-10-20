e123471d8126cb00fd95752d9948646a8cf32056
Eduardo Jauch <jauch.github@gmail.com>
2013-10-20 16:48:02 +0100

    Added main.cpp (Parallelism/Reference project). It's empty. Just for test integration with Netbeans 7.4

---
+++ Parallelization/Reference/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: jauch
+ *
+ * Created on 20 de Outubro de 2013, 16:45
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
